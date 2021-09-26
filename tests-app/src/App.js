c1846ecb4b98867960275d9796ca50095a463694
Dario Rodrigo Torres Messenzani <54510225+Rodrigo00909@users.noreply.github.com>
2021-09-25 23:40:09 -0300

    cambios

--- tests-app/src/App.js
+++ tests-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola {nombre} !!
+          Hola {nombre} !! ¿Cómo te va?
         </p>
         <a
           className="App-link"
